f7fdf5a10ff364a088d94db91173216be639bf6b
CristianG20 <cguerreroasc2001@gmail.com>
2025-05-02 17:53:47 -0500

    Arreglo por indentacion

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ if hist_button:  # Al hacer clic en el botón
 # Crear boton para construir grafico de dispersion.
 disp_button = st.button('Construir histograma') # crear un botón
         
-    if disp_button: # al hacer clic en el botón
-        # escribir un mensaje
-        st.write('Creación de un grafico de dispersion para el conjunto de datos del odometro y la venta de coches')
+if disp_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un grafico de dispersion para el conjunto de datos del odometro y la venta de coches')
             
-        # crear un grafico de dispersion
-        fig = px.sccater(car_data, x="odometer", y = "price")
+    # crear un grafico de dispersion
+    fig = px.sccater(car_data, x="odometer", y = "price")
         
-        # mostrar un gráfico Plotly interactivo
-        st.plotly_chart(fig, use_container_width=True)
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
